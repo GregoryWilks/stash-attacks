2ea33ceb6656e381254b480351ac1acefe2d015d
GregoryWilks <gregwilks1990@gmail.com>
2025-06-18 21:21:54 -0700

    Fix hydration issues and add meta

--- app/story/page.tsx
+++ app/story/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link"
 import Image from "next/image"
 import { Button } from "@/components/ui/button"
+import CurrentYear from "@/components/current-year"
 
 export default function StoryPage() {
   return (
@@ -270,7 +271,7 @@ export default function StoryPage() {
                 </svg>
               </Link>
             </div>
-            <p>&copy; {new Date().getFullYear()} Anastasia's HR Contracting. All rights reserved.</p>
+            <p>&copy; <CurrentYear /> Anastasia's HR Contracting. All rights reserved.</p>
             <div className="flex space-x-4">
               <Link href="/privacy-policy" className="text-white hover:text-[#f69358]">
                 Privacy Policy
